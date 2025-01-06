fd0d5437b5febb37670c3131c94ac519dbc3e68d
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-01-06 15:07:03 +0100

    feat: implement fill_rect in JavaScript

--- main.c
+++ main.c
@@ -7,10 +7,14 @@ typedef struct {
   uint8_t a;
 } color_t;
 
+const color_t BACKGROUND_COLOR = {0x18, 0x18, 0x18, 0xFF};
+
 extern void set_canvas_size(int width, int height);
 extern void clear_with_color(color_t color);
+extern void fill_rect(int x, int y, int w, int h, color_t color);
 
 int main(void) {
   set_canvas_size(800, 600);
-  clear_with_color((color_t){0xAA, 0xBB, 0xCC, 0xDD});
+  clear_with_color(BACKGROUND_COLOR);
+  fill_rect(0, 0, 32, 32, (color_t){0xFF, 0, 0, 0xFF});
 }
--- main.mjs
+++ main.mjs
@@ -1,5 +1,14 @@
 const ctx = document.getElementById("game").getContext("2d");
 
+function parseColor(color) {
+  const r = ((color >> (8 * 0)) & 0xff).toString(16).padStart(2, 0);
+  const g = ((color >> (8 * 1)) & 0xff).toString(16).padStart(2, 0);
+  const b = ((color >> (8 * 2)) & 0xff).toString(16).padStart(2, 0);
+  const a = ((color >> (8 * 3)) & 0xff).toString(16).padStart(2, 0);
+
+  return "#" + r + g + b + a;
+}
+
 async function init() {
   const { instance } = await WebAssembly.instantiateStreaming(
     fetch("./main.wasm"),
@@ -13,21 +22,15 @@ async function init() {
           const memory = instance.exports.memory;
           const colorValue = new Uint32Array(memory.buffer, color, 1)[0];
 
-          const r = ((colorValue >> (8 * 0)) & 0xff)
-            .toString(16)
-            .padStart(2, 0);
-          const g = ((colorValue >> (8 * 1)) & 0xff)
-            .toString(16)
-            .padStart(2, 0);
-          const b = ((colorValue >> (8 * 2)) & 0xff)
-            .toString(16)
-            .padStart(2, 0);
-          const a = ((colorValue >> (8 * 3)) & 0xff)
-            .toString(16)
-            .padStart(2, 0);
-
-          ctx.fillStyle = "#" + r + g + b + a;
+          ctx.fillStyle = parseColor(colorValue);
           ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        },
+        fill_rect: function (x, y, w, h, color) {
+          const memory = instance.exports.memory;
+          const colorValue = new Uint32Array(memory.buffer, color, 1)[0];
+
+          ctx.fillStyle = parseColor(colorValue);
+          ctx.fillRect(x, y, w, h);
         },
       },
     },
