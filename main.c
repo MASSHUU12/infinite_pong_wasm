3d023ed46626c7025c2f61e2a659d24e20631c4d
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-01-06 14:24:27 +0100

    feat: set background color

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-extern int set_canvas_size(int width, int height);
+typedef unsigned char uint8_t;
 
-int main(void)
-{
+typedef struct {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  uint8_t a;
+} color_t;
+
+extern void set_canvas_size(int width, int height);
+extern void clear_with_color(color_t color);
+
+int main(void) {
   set_canvas_size(800, 600);
+  clear_with_color((color_t){0xAA, 0xBB, 0xCC, 0xDD});
 }
--- main.mjs
+++ main.mjs
@@ -9,6 +9,26 @@ async function init() {
           ctx.canvas.width = w;
           ctx.canvas.height = h;
         },
+        clear_with_color: function (color) {
+          const memory = instance.exports.memory;
+          const colorValue = new Uint32Array(memory.buffer, color, 1)[0];
+
+          const r = ((colorValue >> (8 * 0)) & 0xff)
+            .toString(16)
+            .padStart(2, 0);
+          const g = ((colorValue >> (8 * 1)) & 0xff)
+            .toString(16)
+            .padStart(2, 0);
+          const b = ((colorValue >> (8 * 2)) & 0xff)
+            .toString(16)
+            .padStart(2, 0);
+          const a = ((colorValue >> (8 * 3)) & 0xff)
+            .toString(16)
+            .padStart(2, 0);
+
+          ctx.fillStyle = "#" + r + g + b + a;
+          ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        },
       },
     },
   );
